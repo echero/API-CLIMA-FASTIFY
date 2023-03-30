a685659ff6ddc657790d4e124f3e4ca45b25a6ba
emcherone <emcherone@teco.com.ar>
2023-03-30 16:31:50 -0300

    feat: Changes controllers and routes. Integrations fastify config

--- controllers/location.controller.js
+++ controllers/location.controller.js
@@ -1,11 +1,10 @@
 const fetch = require('cross-fetch')
 
 const getLocation = async (req, reply) => {
-    const ip = req.ip
-    const response = await fetch('http://ip-api.com/json')
-        const info = await response.json()
-        reply.send(ip)
-        // reply.send(info)
+    const ip = req.params.ip ? req.params.ip : req.ip
+    const response = await fetch(`http://ip-api.com/json/${ip}`)
+    const info = await response.json()
+    reply.send(info)
 }
 
 module.exports = {
